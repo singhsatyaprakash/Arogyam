24f5f52a4538cfa18ee294228e75c8c3f8a0a639
singhsatyaprakash <singhsatyaprakash70675@gmail.com>
2026-03-14 12:45:14 +0530

    Video call socket setup done

--- backend/Services/videoSocket.js
+++ backend/Services/videoSocket.js
@@ -15,7 +15,7 @@ const attachVideoSocketHandlers = (io, socket) => {
   });
   
   socket.on('user:call',({to,offer})=>{
-    io.to(to).emit("incomming:call",{from: socket.id,offer});
+    io.to(to).emit("incoming:call",{from: socket.id,offer});
   });
 
   socket.on("call:answer",({to,ans})=>{
@@ -23,11 +23,17 @@ const attachVideoSocketHandlers = (io, socket) => {
   });
 
   socket.on("peer:nego:needed",({to,offer})=>{
-    io.to(to).emit("peer:nego:done", {from: socket.id, offer});
+    io.to(to).emit("peer:nego:needed", {from: socket.id, offer});
   });
+
   socket.on("peer:nego:done",({to,ans})=>{
     io.to(to).emit("peer:nego:final", {from: socket.id, ans});
-  })
+  });
+
+  socket.on("peer:ice:candidate", ({ to, candidate }) => {
+    if (!to || !candidate) return;
+    io.to(to).emit("peer:ice:candidate", { from: socket.id, candidate });
+  });
   
 
 
