1def4272698849dab64d66b1f7a9fed2a1466281
singhsatyaprakash <singhsatyaprakash70675@gmail.com>
2026-03-15 18:33:49 +0530

    fix peer

---
+++ client/src/doctorComponent/AddMedicineToPatientList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AddMedicineToPatientList = () => {
+  return (
+    <div className="p-4 bg-gray-50 rounded-lg min-h-[200px] flex items-center justify-center text-gray-500">
+      <p className="text-center">
+        Medicine prescription component will be implemented here.<br />
+        (Placeholder for future logic)
+      </p>
+    </div>
+  );
+};
+
+export default AddMedicineToPatientList;
--- client/src/services/peer.js
+++ client/src/services/peer.js
@@ -13,21 +13,21 @@ class PeerService{
     }
     async getAnswer(offer){
         if(this.peer){
-            await this.peer.setRemoteDescription(new RTCSessionDescription(offer));
+            await this.peer.setRemoteDescription(offer);
             const ans = await this.peer.createAnswer();
-            await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+            await this.peer.setLocalDescription(ans);
             return ans;
         }
     }
     async setLocalDescription(ans){
         if(this.peer){
-            await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
+            await this.peer.setRemoteDescription(ans);
         }
     }
     async getOffer(){
         if(this.peer){
             const offer = await this.peer.createOffer();
-            await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+            await this.peer.setLocalDescription(offer);
             return offer;
         }
     }
