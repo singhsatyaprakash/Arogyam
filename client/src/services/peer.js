4a14d2a5c390ffcdac8a08bf40a7c1294b5a01b0
singhsatyaprakash <singhsatyaprakash70675@gmail.com>
2026-03-18 09:28:58 +0530

    peer fix

--- client/src/services/peer.js
+++ client/src/services/peer.js
@@ -5,7 +5,7 @@ class PeerService{
                 iceServers:[{
                     urls:[
                         'stun:stun.l.google.com:19302',
-                        'stun:global.stun.twilo.com:3478',
+                        'stun:global.stun.twilio.com:3478',
                     ],
                 }],
             });
